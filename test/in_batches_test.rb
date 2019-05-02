4473525601020510ac1770eb7baf413ba656fa2f
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-05-02 17:02:03 +0800

    add test cases

--- test/in_batches_test.rb
+++ test/in_batches_test.rb
@@ -9,4 +9,27 @@ class InBatchesTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::InBatches::VERSION
   end
+
+  def test_update_all
+    in_sandbox do
+      assert_queries(2){ User.in_batches.update_all('money = money + 1') }
+      assert_equal [101, 201, 1], User.order(:id).pluck(:money)
+    end
+  end
+
+  def test_update_all_with_low_batch_num
+    in_sandbox do
+      assert_queries(4){ User.in_batches(of: 2).update_all('money = money + 1') }
+      assert_equal [101, 201, 1], User.order(:id).pluck(:money)
+    end
+  end
+
+  def test_block
+    User.in_batches(of: 2).each_with_index do |users, index|
+      case index
+      when 0 ; assert_equal [100, 200], users.map(&:money)
+      when 1 ; assert_equal [0], users.map(&:money)
+      end
+    end
+  end
 end
