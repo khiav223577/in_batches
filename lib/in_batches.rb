791a067112d6c6a68080d491d8dfdf76d7c78365
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-08-25 00:08:24 +0800

    Add warning if on rails 5/6

--- lib/in_batches.rb
+++ lib/in_batches.rb
@@ -2,4 +2,9 @@
 
 require 'in_batches/version'
 require 'active_record'
-require 'in_batches/active_record_extensions' if not defined?(ActiveRecord::Batches::BatchEnumerator)
+
+if defined?(ActiveRecord::Batches::BatchEnumerator)
+  warn "Congratulations on upgrading Rails to 5 or above. You could remove `in_batches` gem now. :)"
+else
+  require 'in_batches/active_record_extensions'
+end
